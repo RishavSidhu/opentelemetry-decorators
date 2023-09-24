08455622d13edb0497c8769450d68d9c966b6244
Rishavjeet Singh Sidhu <rishavsidhu13@gmail.com>
2023-09-24 11:15:37 -0400

    updated dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='opentelemetry-decorators',
     version='23.09.23',
     packages=['opentelemetry', 'opentelemetry.decorators', 'opentelemetry.decorators.handlers'],
+    install_requires=[
+        'opentelemetry-api',
+        'opentelemetry-sdk'
+    ],
     url='',
     license='',
     author='Rishavjeet Singh Sidhu',
